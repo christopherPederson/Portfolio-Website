d922867d047fbc96c7f9c0dfb48c2689198a334c
christopherPederson <127063705+christopherPederson@users.noreply.github.com>
2024-03-06 10:47:32 -0500

    finished 404 page

--- portfolio-website-v.0/src/pages/Error404.js
+++ portfolio-website-v.0/src/pages/Error404.js
@@ -2,7 +2,7 @@ import BtnPrimary from "./components/btn--primary";
 
 export default  function Error404() {
     return (
-        <div className="text-2xl flex flex-col justify-center items-center text-center h-screen">
+        <div className="text-2xl h-full flex flex-col justify-center items-center text-center w-9/12 gap-8">
             <h1 className="text-5xl">:\ Error 404</h1>
             <p>Oh! looks like you found a page that doesn't exist on my site. Go ahead and return to my home page to explore more.</p>
             <BtnPrimary props={{href: "/", purpose: "Return Home"}} />
--- portfolio-website-v.0/src/pages/components/btn--primary.js
+++ portfolio-website-v.0/src/pages/components/btn--primary.js
@@ -1,8 +1,8 @@
 export default function BtnPrimary({ props }) {
     return (
         <a href={props.href}>
-            <button className="bg-black hover:bg-white hover:text-black text-white py-2 px-4 rounded-md border border-white">
-                {props.purpose}
+            <button className="bg-black hover:bg-white hover:text-black text-white py-2 px-4 rounded-md border-2 border-white">
+                <u>{props.purpose}</u>
             </button>
         </a>
     );
