2c13a6961d27a677e97f11637c55ba6d1dcfbba1
christopherPederson <127063705+christopherPederson@users.noreply.github.com>
2024-03-25 23:02:31 -0400

    added and styled linked icons

--- portfolio-website-v.0/src/components/BannerComponents/IntroText.js
+++ portfolio-website-v.0/src/components/BannerComponents/IntroText.js
@@ -1,17 +1,26 @@
+import LinkedIcon from "../GlobalComponents/LinkedIcon";
+import LinkedIconData from "../data/LinkedIcons.json";
+
 export default function IntroText() {
     return (
-        <div className="flex flex-col justify-evenly p-2">
+        <div className="flex flex-col justify-evenly p-2 h-screen">
             <h1 className="text-5xl">
                 Hi, im Chris. Welcome to my custom coded portfolio website...
             </h1>
-            <p className="">
+            <p className="text-2xl">
                 This site was made to demonstrate not only my Full stack
                 programming skills but also my plethora of hardware and software
                 engineering projects
             </p>
-            <div>
-                <a></a>
-            </div>
+            <div className="flex h-14">{LinkedIconData.icons.map((NewIcon, index) => {
+                return (
+                    <LinkedIcon
+                        iconURL={NewIcon.iconURL}
+                        name={NewIcon.name}
+                        link={NewIcon.link}
+                    />
+                );
+            })}</div>
         </div>
     );
 }
--- portfolio-website-v.0/src/components/GlobalComponents/LinkedIcon.js
+++ portfolio-website-v.0/src/components/GlobalComponents/LinkedIcon.js
@@ -1,7 +1,24 @@
+import "../ComponentStyles/LinkedIcon.css";
+import { useState } from "react";
+
 export default function LinkedIcon(props) {
+    let [icon, setIcon] = useState(null);
+
+    console.log("Importing icon: ", props.iconURL);
+
+    import(`../data/GlobalImages/${props.iconURL}`)
+        .then((newIcon) => {
+            setIcon(newIcon.default);
+        })
+        .catch((error) => {
+            console.log("Error importing icon: ", error);
+        });
+
     return (
-        <>
-        <a><img src/></a>
-        </>
-    )
+        <div className="IconWrapper" data-tooltip={props.name}>
+            <a href={props.link}>
+                <img src={icon} />
+            </a>
+        </div>
+    );
 }
