b0df8f8d4161e1dd63e060002416bfd0cc701d79
christopherPederson <127063705+christopherPederson@users.noreply.github.com>
2024-04-03 10:20:29 -0400

    made project component dynamic rendering more efficient

--- portfolio-website-v.0/src/components/BodyComponents/Project.js
+++ portfolio-website-v.0/src/components/BodyComponents/Project.js
@@ -1,37 +1,15 @@
 import { useState, useEffect } from "react";
 
 export default function Project(props) {
-    // declare utilities
-    let data = props.projectData; // data is the projectData object passed in from Slice.js
-
     // declare state variables
     let [mainImage, setMainImage] = useState(null); // mainImage is the first image to be displayed in the project
-
-    let content_left = (
-        <div
-            id={props.uid}
-            className="projectWrapper"
-            style={{ backgroundColor: data.color }}
-            onMouseEnter={props.onMouseEnter}
-            onMouseLeave={props.onMouseLeave}
-        >
-            <img src={mainImage} alt={data.name} />
-            <h1>{data.title}</h1>
-        </div>
-    );
-    let content_right = (
-        <div
-            id={props.uid}
-            className="projectWrapper"
-            style={{ backgroundColor: data.color }}
-            onMouseEnter={props.onMouseEnter}
-            onMouseLeave={props.onMouseLeave}
-        >
-            <h1>{data.title}</h1>
-            <img src={mainImage} alt={data.name} />
-        </div>
-    );
-    let content = <div id={props.uid} className="projectWrapper"></div>;
+    // declare utilities
+    let data = props.projectData; // data is the projectData object passed in from Slice.js
+    let elementAry = [
+        <img src={mainImage} alt={data.name} />,
+        <h1>{data.title}</h1>,
+    ];
+    const contentOrientation = data.orientation === "image--left" ? 0 : 1;
 
     import(
         process.env.PUBLIC_URL +
@@ -45,7 +23,20 @@ export default function Project(props) {
             console.error("Error importing image: ", error);
         });
 
-    content = data.orientation === "image--left" ? content_left : content_right;
+    let generateContent = () => {
+        return (
+            <div
+                id={props.uid}
+                className="projectWrapper"
+                style={{ backgroundColor: data.color }}
+                onMouseEnter={props.onMouseEnter}
+                onMouseLeave={props.onMouseLeave}
+            >
+                {elementAry[contentOrientation]}
+                {elementAry[contentOrientation ^ 1]}
+            </div>
+        );
+    };
 
-    return content;
+    return generateContent();
 }
