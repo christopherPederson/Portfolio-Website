e613c7eeb1c93ac32d2cfb9508c6744e3d5f738c
christopherPederson <127063705+christopherPederson@users.noreply.github.com>
2024-04-03 00:03:48 -0400

    worked on slice and project components

--- portfolio-website-v.0/src/components/BodyComponents/Project.js
+++ portfolio-website-v.0/src/components/BodyComponents/Project.js
@@ -3,49 +3,17 @@ import { useState, useEffect } from "react";
 export default function Project(props) {
     // declare utilities
     let data = props.projectData; // data is the projectData object passed in from Slice.js
-    let projectWrapper = document.getElementById(props.uid); // projectWrapper is the div element that contains the project
 
     // declare state variables
     let [mainImage, setMainImage] = useState(null); // mainImage is the first image to be displayed in the project
-    let [inFocus, setInFocus] = useState(false); // inFocus is a boolean to determine if the project is in focus
-
-    //declare event listeners
-    useEffect(() => {
-        const projectWrapper = document.getElementById(props.uid);
-        if (projectWrapper) {
-            const handleMouseOver = () => {
-                setInFocus(true);
-            };
-            const handleMouseOut = () => {
-                setInFocus(false);
-            };
-
-            projectWrapper.addEventListener("mouseover", handleMouseOver);
-            projectWrapper.addEventListener("mouseout", handleMouseOut);
-
-            return () => {
-                projectWrapper.removeEventListener(
-                    "mouseover",
-                    handleMouseOver
-                );
-                projectWrapper.removeEventListener("mouseout", handleMouseOut);
-            };
-        }
-    }, []);
-
-    // declare event handlers
-    const handleMouseOver = () => {
-        setInFocus(true);
-    };
-    const handleMouseOut = () => {
-        setInFocus(false);
-    };
 
     let content_left = (
         <div
             id={props.uid}
             className="projectWrapper"
             style={{ backgroundColor: data.color }}
+            onMouseEnter={props.onMouseEnter}
+            onMouseLeave={props.onMouseLeave}
         >
             <img src={mainImage} alt={data.name} />
             <h1>{data.title}</h1>
@@ -56,13 +24,13 @@ export default function Project(props) {
             id={props.uid}
             className="projectWrapper"
             style={{ backgroundColor: data.color }}
+            onMouseEnter={props.onMouseEnter}
+            onMouseLeave={props.onMouseLeave}
         >
             <h1>{data.title}</h1>
             <img src={mainImage} alt={data.name} />
         </div>
     );
-    let content_hover = <div id={props.uid} className="projectWrapper"></div>;
-
     let content = <div id={props.uid} className="projectWrapper"></div>;
 
     import(
@@ -77,12 +45,7 @@ export default function Project(props) {
             console.error("Error importing image: ", error);
         });
 
-    if (inFocus) {
-        content = content_hover;
-    } else {
-        content =
-            data.orientation === "image--left" ? content_left : content_right;
-    }
+    content = data.orientation === "image--left" ? content_left : content_right;
 
     return content;
 }
--- portfolio-website-v.0/src/components/BodyComponents/Slice.js
+++ portfolio-website-v.0/src/components/BodyComponents/Slice.js
@@ -4,12 +4,51 @@ import "./Slice.css";
 import { v4 as uuidv4 } from "uuid";
 
 export default function Slice(props) {
-    let data = props.sliceData.projects;
+    const data = props.sliceData.projects;
+    const [projectElements, setProjectElements] = useState([]);
+    const projectUIDs = [uuidv4(), uuidv4()];
+
+    useEffect(() => {
+        const elements = [
+            document.getElementById(projectUIDs[0]),
+            document.getElementById(projectUIDs[1]),
+        ];
+        setProjectElements(elements);
+
+        return () => {
+            elements.forEach((element, index) => {
+                if (element) {
+                    element.removeEventListener("mouseover", () =>
+                        handleMouseOver(index)
+                    );
+                    element.removeEventListener("mouseout", () =>
+                        handleMouseOut(index)
+                    );
+                }
+            });
+        };
+    }, []);
+
+    const handleMouseOver = (index) => {
+        projectElements[index].classList.add("maximize");
+        projectElements[index ^ 1].classList.add("minimize");
+    };
+
+    const handleMouseOut = (index) => {
+        projectElements[index].classList.remove("maximize");
+        projectElements[index ^ 1].classList.remove("minimize");
+    };
 
     const generateProjects = () => {
-        return data.map((element, index) => {
-            return <Project key={uuidv4()} uid={uuidv4()} projectData={data[index]} />;
-        });
+        return data.map((project, index) => (
+            <Project
+                key={uuidv4()}
+                uid={projectUIDs[index]}
+                projectData={project}
+                onMouseEnter={() => handleMouseOver(index)}
+                onMouseLeave={() => handleMouseOut(index)}
+            />
+        ));
     };
 
     return <div className="slice">{generateProjects()}</div>;
