bde410ca02e3830da8240e20b06a2a1c435ff5a0
christopherPederson <127063705+christopherPederson@users.noreply.github.com>
2024-04-22 13:25:29 -0400

    beginning of custom calander

--- portfolio-website-v.0/src/components/BodyComponents/GithubCalendarComp.js
+++ portfolio-website-v.0/src/components/BodyComponents/GithubCalendarComp.js
@@ -1,15 +1,19 @@
-import React, { useEffect } from 'react';
-import GitHubCalendar from './GithubCalendar';
-import './github-calendar-responsive.css';
+import React, { useEffect } from "react";
+import GitHubCalendar from "./GithubCalendar";
+import "./github-calendar.css";
 
 const GitHubCalendarComponent = ({ username }) => {
     useEffect(() => {
-        new GitHubCalendar('.calendar', "christopherPederson", { responsive: true });
+        new GitHubCalendar(".calendar", "christopherPederson", {
+            responsive: false,
+            global_stats: false,
+        });
     }, [username]);
 
     return (
-        <div className="calendar">
-            Loading data...
+        <div className="calendarWrapper">
+            <h1>My GitHub Commit History</h1>
+            <div className="calendar">Loading data...</div>
         </div>
     );
 };
