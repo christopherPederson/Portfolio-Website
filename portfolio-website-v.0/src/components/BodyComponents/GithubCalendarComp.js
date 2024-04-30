638ccb7a1c8a4dc2aae227eddda0c7482545c77b
christopherPederson <127063705+christopherPederson@users.noreply.github.com>
2024-04-29 21:29:23 -0600

    github calendar working and styled

--- portfolio-website-v.0/src/components/BodyComponents/GithubCalendarComp.js
+++ portfolio-website-v.0/src/components/BodyComponents/GithubCalendarComp.js
@@ -1,19 +1,22 @@
-import React, { useEffect } from "react";
-import GitHubCalendar from "./GithubCalendar";
-import "./github-calendar-responsive.css";
+import { useEffect } from "react";
+import GitHubCalendar from "react-github-calendar";
+import "./GithubCalendarComp.css"
 
-export default function GitHubCalendarComponent(username) {
+export default function GitHubCalendarComponent(props) {
     useEffect(() => {
-        new GitHubCalendar(".calendar", "christopherPederson", {
-            responsive: true,
-            global_stats: true,
-            tooltips:true,
-        });
-    }, [username]);
+        const calendarDayElements = document.getElementsByTagName("rect");
 
+        // Check if calendarDayElements is not empty
+        if (calendarDayElements.length > 0 && calendarDayElements) {
+            // Apply style to each element
+            Array.from(calendarDayElements).forEach(element => {
+                element.style.width = "100%";
+            });
+        }
+    }, []);
     return (
-        <div className="calendarWrapper">
-            <div className="calendar">Loading data...</div>
+        <div id="calendarWrapper">
+            <GitHubCalendar username={props.username} id="calendar" />
         </div>
     );
 }
